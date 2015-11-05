17a603314bc6d34e21b086fa8ce89f721ce7c68f
Cyril Ferlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2015-11-05 22:38:18 +0100

    Improved Customer and begun fly.

--- src/domaine/Customer.java
+++ src/domaine/Customer.java
@@ -1,9 +1,35 @@
 package domaine;
 
+import domaine.Destination.City;
+
+import java.time.LocalDate;
+import java.time.Period;
+
 /**
  * I am a class that describe a customer.
  *
  * @author Cyril Ferlicot & Aurelien Rousseau
  */
 public class Customer {
+
+    protected String firstName;
+
+    protected String lastName;
+
+    protected LocalDate birthday;
+
+    protected City city;
+
+    public Customer(String firstName, String lastName, LocalDate birthday, City city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.city = city;
+    }
+
+    public Integer age() {
+        return Period.between(this.birthday, LocalDate.now()).getYears();
+    }
+
+
 }
---
+++ src/domaine/Fly.java
@@ -0,0 +1,20 @@
+package domaine;
+
+import domaine.Destination.City;
+
+/**
+ * I am a class that describe a fly.
+ *
+ * @author Cyril Ferlicot & Aurelien Rousseau
+ */
+public class Fly {
+
+    protected final City origin;
+
+    protected final City destination;
+
+    public Fly(City origin, City destination){
+        this.origin = origin;
+        this.destination = destination;
+    }
+}

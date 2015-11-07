2131d168a2406a5e65cb5d4cf6caa93b54604050
Cyril Ferlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2015-11-07 21:13:19 +0100

    added driver and config files for db

--- ressources/BDD/bdd.sql
+++ ressources/BDD/bdd.sql
@@ -1,5 +1,6 @@
 -- I am a script to create the database
 
+/* We can't use this with alwaysdata.
 CREATE DATABASE "Agency"
   WITH OWNER = postgres
   ENCODING = 'UTF8'
@@ -7,3 +8,4 @@ CREATE DATABASE "Agency"
   LC_COLLATE = 'French_France.1252'
   LC_CTYPE = 'French_France.1252'
   CONNECTION LIMIT = -1;
+*/
--- ressources/BDD/dropbdd.sql
+++ ressources/BDD/dropbdd.sql
@@ -1,3 +1,5 @@
 -- I am a script to drop the data base
 
+/* We can't use this with always data.
 DROP DATABASE "Agency";
+*/

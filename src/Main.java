9caff346816ea3d37d55d87ff81b3865d6490034
Cyril Ferlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2015-12-14 22:09:55 +0100

    Do main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import gui.AgencyGUI;
+
 import java.io.IOException;
 
 /**
@@ -5,27 +7,6 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-/*
-        Customer cust = new Customer(null, "foo", "bar", LocalDate.of(1993, 8, 20), new City("Galifrey"));
-        try {
-            DAO<Customer> custDAO = new CustomerDAO();
-            // Add
-            cust = custDAO.create(cust);
-            // Get
-            System.out.println(custDAO.find(cust.id()));
-            cust.firstName("Baracuda!");
-            // Update
-            custDAO.update(cust);
-            // Delete
-            custDAO.delete(cust);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        MainInteractions interaction = (new MainInteractions(br));
-        while (true) {
-            interaction.interact();
-        }*/
+        new AgencyGUI();
     }
 }

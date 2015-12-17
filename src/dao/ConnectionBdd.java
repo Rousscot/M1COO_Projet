2705bbf856510f51e19fe3d729ad762b9251194c
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2015-12-17 14:21:46 +0100

    Connection for fac

--- src/dao/ConnectionBdd.java
+++ src/dao/ConnectionBdd.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,11 +16,11 @@ import java.sql.SQLException;
  */
 public class ConnectionBdd {
 
-    protected String url = "jdbc:postgresql://postgresql1.alwaysdata.com:5432/cestpasfaux_agency";
+    protected String url;
 
-    protected String user = "cestpasfaux";
+    protected String user;
 
-    protected String passwd = "cotelette";
+    protected String passwd;
 
     protected static Connection connection;
 
@@ -25,16 +28,43 @@ public class ConnectionBdd {
      * I am the constructor. I also check that the connection works.
      */
     private ConnectionBdd() {
+        setHomeConnection();
+    }
+
+    public void setHomeConnection() {
         try {
             //I just try to instantiate the driver to check that the driver is here.
             Class.forName("org.postgresql.Driver");
             System.out.println("Driver O.K.");
+            url = "jdbc:postgresql://postgresql1.alwaysdata.com:5432/cestpasfaux_agency";
+            user = "cestpasfaux";
+            passwd = "cotelette";
             connection = DriverManager.getConnection(url, user, passwd);
             System.out.println("Connection O.K.");
         } catch (ClassNotFoundException e) {
             System.out.println("There is a problem with the instantiation of the driver. Check that the driver is on your classpath.");
             System.exit(10);
         } catch (SQLException e) {
+            System.out.println("There is a problem with the url/login/password of the database :(");
+            System.exit(15);
+        }
+    }
+
+    public void setFacConnection(){
+        try {
+            //I just try to instantiate the driver to check that the driver is here.
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Driver O.K.");
+
+            url = "jdbc:postgresql://webtp.fil.univ-lille1.fr:5432/ferlicotdelbe";
+            user = "ferlicotdelbe";
+            passwd = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+            connection = DriverManager.getConnection(url, user, passwd);
+            System.out.println("Connection O.K.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("There is a problem with the instantiation of the driver. Check that the driver is on your classpath.");
+            System.exit(10);
+        } catch (SQLException | IOException e) {
             System.out.println("There is a problem with the url/login/password of the database :(");
             System.exit(15);
         }

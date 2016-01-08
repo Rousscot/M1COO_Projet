7a0d9bbef1a4d5bbfe7e807cc05b568968da841d
Cyril Ferlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-01-08 18:32:59 +0100

    Try tool generation of Intellij

---
+++ src/gui/Test.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by JeCisC on 08/01/2016.
+ */
+public class Test {
+    private JList list1;
+    private JList list2;
+    private JComboBox comboBox1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}

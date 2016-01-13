72624ba2c11f02e85ec864aae6172278936efaa9
Cyril Ferlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-01-13 21:48:46 +0100

    Try with date picker ?

--- src/gui/editableListPanels/CustomerForm.java
+++ src/gui/editableListPanels/CustomerForm.java
@@ -5,10 +5,15 @@ import domaine.destination.City;
 import domaine.exception.BirthdayFormatException;
 import factory.Agency;
 import gui.model.CitiesDataSource;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Properties;
 
 /**
  * Created by aurelien on 10/01/2016.
@@ -23,50 +28,95 @@ public class CustomerForm extends AbstractForm<Customer> {
     protected JTextField lastNameField;
     protected JList<City> cities;
 
+    protected JDatePickerImpl birthdayField;
+    protected static final String LABELBIRTHDAY = "Date de naissance";
 
-    protected JPanel birthdayPanel;
-    protected static final String BDLABEL = "Jour de naissance :";
-    protected JTextField dayField;
-    protected static final String BMLABEL = "Mois de naissance :";
-    protected JTextField monthField;
-    protected static final String BYLABEL = "Année de naissance :";
-    protected JTextField yearField;
 
     public CustomerForm() {
         super();
+        initSpacers();
+    }
+
+    public void initSpacers() {
+        GridBagConstraints gbc;
+        JPanel spacer1 = new JPanel();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        this.add(spacer1, gbc);
+        JPanel spacer2 = new JPanel();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        this.add(spacer2, gbc);
+        JPanel spacer3 = new JPanel();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 5;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(spacer3, gbc);
     }
 
     @Override
     //TODO
     protected LayoutManager getFormLayout() {
-        return new GridLayout(1, 7);
-    }
-
-    protected void initPanel() {
-        birthdayPanel = new JPanel();
-        birthdayPanel.setLayout(new GridLayout(3, 1));
-        configureBirthdayTabbedPanel();
+        return new GridBagLayout();
     }
 
     @Override
     protected void initLabels() {
-        initPanel();
-        configureNameTabbedPanel();
-        initCityTap();
-        add(birthdayPanel);
     }
 
     @Override
     protected void initTextFields() {
-        lastNameField = new JTextField();
-        lastNameField.setColumns(10);
+        JTabbedPane fnTabbedPanel = new JTabbedPane();
+        GridBagConstraints gbc;
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        this.add(fnTabbedPanel, gbc);
+        firstNameField = new JTextField();
+        fnTabbedPanel.addTab(FNLABEL, firstNameField);
 
-        dayField = new JTextField();
-        dayField.setColumns(10);
-        monthField = new JTextField();
-        monthField.setColumns(10);
-        yearField = new JTextField();
-        yearField.setColumns(10);
+        JTabbedPane lnTabbedPanel = new JTabbedPane();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        this.add(lnTabbedPanel, gbc);
+        lastNameField = new JTextField();
+        lnTabbedPanel.addTab(LNLABEL, lastNameField);
+
+        initCityTap();
+
+        JTabbedPane bdTabbedPanel = new JTabbedPane();
+        JPanel birthdayPanel = new JPanel();
+        bdTabbedPanel.addTab(LABELBIRTHDAY, birthdayPanel);
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 5;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(bdTabbedPanel, gbc);
+        birthdayPanel.setLayout(new BorderLayout());
+        UtilDateModel model = new UtilDateModel();
+        Properties p = new Properties();
+        p.put("text.today", "Today");
+        p.put("text.month", "Month");
+        p.put("text.year", "Year");
+        JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+        birthdayField = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+        birthdayPanel.add(birthdayField);
+
     }
 
     @Override
@@ -74,9 +124,7 @@ public class CustomerForm extends AbstractForm<Customer> {
         firstNameField.setText("");
         lastNameField.setText("");
         selectFirstElementIfPossible(cities);
-        dayField.setText("");
-        monthField.setText("");
-        yearField.setText("");
+        birthdayField.getModel().setDate(1, 1, 2000);
     }
 
     public void selectFirstElementIfPossible(JList list) {
@@ -106,7 +154,13 @@ public class CustomerForm extends AbstractForm<Customer> {
     public void initCityTap() {
         System.out.println(); //TODO retirer affichage
         JTabbedPane cityTabbedPanel = new JTabbedPane();
-        this.add(cityTabbedPanel);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 4;
+        gbc.gridy = 2;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        this.add(cityTabbedPanel, gbc);
         JPanel cityPanel = new JPanel();
         cityPanel.setLayout(new BorderLayout(0, 0));
         cityTabbedPanel.addTab("Ville :", cityPanel);
@@ -114,37 +168,6 @@ public class CustomerForm extends AbstractForm<Customer> {
         cityPanel.add(cityScrollPane, BorderLayout.CENTER);
         cities = new JList<>();
         cityScrollPane.setViewportView(cities);
-    }
-
-
-    public void configureNameTabbedPanel() {
-        JTabbedPane fnTabbedPanel = new JTabbedPane();
-        this.add(fnTabbedPanel);
-        firstNameField = new JTextField();
-        fnTabbedPanel.addTab(FNLABEL, firstNameField);
-
-        JTabbedPane lnTabbedPanel = new JTabbedPane();
-        this.add(lnTabbedPanel);
-        lastNameField = new JTextField();
-        lnTabbedPanel.addTab(LNLABEL, lastNameField);
-    }
-
-    public void configureBirthdayTabbedPanel() {
-        JTabbedPane bdTabbedPanel = new JTabbedPane();
-        birthdayPanel.add(bdTabbedPanel);
-        dayField = new JTextField();
-        bdTabbedPanel.addTab(BDLABEL, dayField);
-
-        JTabbedPane bmTabbedPanel = new JTabbedPane();
-        birthdayPanel.add(bmTabbedPanel);
-        monthField = new JTextField();
-        bmTabbedPanel.addTab(BMLABEL, monthField);
-
-        JTabbedPane byTabbedPanel = new JTabbedPane();
-        birthdayPanel.add(byTabbedPanel);
-        yearField = new JTextField();
-        byTabbedPanel.addTab(BYLABEL, yearField);
-
     }
 
     @Override
@@ -160,14 +183,8 @@ public class CustomerForm extends AbstractForm<Customer> {
         refresh();
         firstNameField.setText(controller.getFirstName());
         lastNameField.setText(controller.getLastName());
-        //TODO selectionne la ville du customer
         cities.setSelectedValue(controller.getCity(), true);
-        String year = controller.getBirthday().getYear() + "";
-        String month = controller.getBirthday().getMonthValue() + "";
-        String day = controller.getBirthday().getDayOfMonth() + "";
-        dayField.setText(day);
-        monthField.setText(month);
-        yearField.setText(year);
+        birthdayField.getModel().setDate(controller.getBirthday().getYear(), controller.getBirthday().getMonthValue(), controller.getBirthday().getDayOfYear() );
     }
 
 
@@ -176,10 +193,7 @@ public class CustomerForm extends AbstractForm<Customer> {
     }
 
     public LocalDate birthday() throws BirthdayFormatException {
-        int year = year();
-        int month = month();
-        int day = day();
-        return LocalDate.of(year, month, day);
+        return LocalDate.parse( new SimpleDateFormat("yyyy-MM-dd").format(birthdayField.getModel().getValue()));
     }
 
     public String firstName() {
@@ -188,31 +202,6 @@ public class CustomerForm extends AbstractForm<Customer> {
 
     public String lastName() {
         return lastNameField.getText().trim();
-    }
-
-    public int year() throws NumberFormatException, BirthdayFormatException {
-        int year = Integer.parseInt(yearField.getText());
-        int currentYear = LocalDate.now().getYear();
-        if (year < (currentYear - 100) || year > currentYear) {
-            throw new BirthdayFormatException("L'année de naissance doit être comprise entre " + (currentYear - 100) + " et " + currentYear);
-        }
-        return year;
-    }
-
-    public int month() throws NumberFormatException, BirthdayFormatException{
-        int month = Integer.parseInt(monthField.getText());
-        if (month < 1 || month > 12) {
-            throw new BirthdayFormatException("Le mois de naissance doit être compris entre 1 et 12");
-        }
-        return month;
-    }
-
-    public int day() throws NumberFormatException, BirthdayFormatException{
-        int day = Integer.parseInt(dayField.getText());
-        if (day < 1 || day > 31) {
-            throw new BirthdayFormatException("Le jour de naissance doit être compris entre 1 et 31");
-        }
-        return day;
     }
 
 }
